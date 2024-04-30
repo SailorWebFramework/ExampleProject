f98d1ca9f482ddc32273ce4523a9fd42bbafed71
Joshua Davis <chess.ofx@gmail.com>
2024-04-30 01:43:16 -0700

    fixed SailorCore vs Sailor name

---
+++ Sources/Elements.swift
@@ -0,0 +1,28 @@
+//
+//  File.swift
+//  
+//
+//  Created by Joshua Davis on 4/24/24.
+//
+
+import Sailor
+
+typealias Header = HTML.Header
+typealias Footer = HTML.Footer
+
+typealias Div = HTML.Div
+typealias Span = HTML.Span
+typealias Img = HTML.Img
+typealias Button = HTML.Button
+
+typealias H1 = HTML.H1
+typealias H2 = HTML.H2
+typealias H3 = HTML.H3
+typealias H4 = HTML.H4
+typealias H5 = HTML.H5
+typealias H6 = HTML.H6
+
+typealias A = HTML.A
+typealias P = HTML.P
+typealias B = HTML.B
+typealias I = HTML.I
---
+++ Sources/Pages/HomePage/HomePage.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  
+//
+//  Created by Joshua Davis on 4/30/24.
+//
+
+import Foundation
